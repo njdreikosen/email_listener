9f260fc59155de1f924201ec6be773ecdbfe4a93
njdreikosen <njdreikosen@wisc.edu>
2020-05-22 22:07:47 -0500

    Remove python versions that aren't passing ci testing from PyPi setup file.

--- setup.py
+++ setup.py
@@ -12,9 +12,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
